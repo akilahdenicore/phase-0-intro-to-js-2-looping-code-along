f35616067be68b8264e2da5a586a8c2ad69597ae
Akilah Snowden <akilahdsnowden@gmail.com>
2022-01-30 16:55:05 -0500

    done with assignment

--- index.js
+++ index.js
@@ -1,1 +1,15 @@
-// Code your solutions in this file
+const names = ["Cora", "Mom", "Grant"];
+ 
+function writeCards(names) {
+   for (let i = 0; i < names.length; i++) {
+       console.log(`Thank you ${names[i]}, for the beautiful gift!`);
+   }
+   return names;
+}
+writeCards(names);
+ 
+
+let countDown = 10;
+while (countDown > -1) {
+    console.log(countDown--);
+}
